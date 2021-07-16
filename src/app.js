2ead48c5cd54684a3ae3c10f5e1de37b8c00c549
tatisavic <83769275+tatisavic@users.noreply.github.com>
2021-07-16 12:03:56 -0400

    updated temp

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-aler("Hello World");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.main.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.main.wind.speed);
+}
+
+let apiKey = "f5ddc1c4d2338589a5fde9611a79fb8b";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
