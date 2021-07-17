8c74bfbea64f0664ede39dde868b57cef097a449
tatisavic <83769275+tatisavic@users.noreply.github.com>
2021-07-17 10:38:28 -0400

    added icon

--- src/app.js
+++ src/app.js
@@ -35,9 +35,16 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.main.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function search(city) {
   let apiKey = "f5ddc1c4d2338589a5fde9611a79fb8b";
+  let city = "New York";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
